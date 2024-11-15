9ec86db3f4a5bcba7ad62d0ae52da47448c67c5c
David Fraser <davidf@inconceivable.co.za>
2024-11-15 13:15:57 +0000

    Actually, we're not curently building our own images except for social-app For social-app, include DOMAIN in the image name, since its branded Don't default to the builder compose file Don't carry on if docker steps fail in step 30
    (cherry picked from commit 4edc255aa13f8d1e6c00a245bbd29de19d5e9ea2)

--- Makefile
+++ Makefile
@@ -57,7 +57,7 @@ passfile ?=${wDir}/config/secrets-passwords.env
 
 # docker-compose file
 f ?=${wDir}/docker-compose.yaml
-f ?=${wDir}/docker-compose-builder.yaml
+# f ?=${wDir}/docker-compose-builder.yaml
 
 # folders of repos
 #_nrepo   :=atproto indigo social-app feed-generator did-method-plc pds ozone
--- step30-deploy-bluesky.sh
+++ step30-deploy-bluesky.sh
@@ -9,13 +9,13 @@ set -o allexport
 set +o allexport
 
 show_heading "Fetching containers" "for required services"
-make docker-pull
+make docker-pull || { show_error "Fetching Containers failed:" "Please see error above" ; exit 1 ; }
 
 show_heading "Deploy required containers" "(database, caddy etc)"
-make docker-start-nowatch
+make docker-start-nowatch || { show_error "Required Containers failed:" "Please see error above" ; exit 1 ; }
 
 show_heading "Deploy bluesky containers" "(plc, bgs, appview, pds, ozone, ...)"
-make docker-start-bsky-nowatch
+make docker-start-bsky-nowatch || { show_error "BlueSky Containers failed:" "Please see error above" ; exit 1 ; }
 
 show_heading "Wait for startup" "of social app"
 # could also wait for Sbsky ?=pds bgs bsky social-app palomar
