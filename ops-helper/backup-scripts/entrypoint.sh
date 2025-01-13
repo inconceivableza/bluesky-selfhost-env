aacc266729f16b0a8e4580e1daad1ca97cfae129
David Fraser <davidf@inconceivable.co.za>
2025-01-13 11:23:03 +0000

    Configure ofelia and run within the container

--- ops-helper/backup-scripts/entrypoint.sh
+++ ops-helper/backup-scripts/entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 # Install required packages
-apk add --no-cache sqlite dcron tzdata postgresql16-client
+apk add --no-cache sqlite dcron tzdata postgresql16-client curl libc6-compat
+cd /staging
+curl -L -O https://github.com/mcuadros/ofelia/releases/download/v0.3.14/ofelia_0.3.14_linux_amd64.tar.gz
+tar xzf ofelia_0.3.14_linux_amd64.tar.gz -C /usr/bin ofelia
+chmod a+x /usr/bin/ofelia
+echo path is $PATH
+ls -l /usr/bin/ofelia
 
-/scripts/backup.sh
+/usr/bin/ofelia validate && /usr/bin/ofelia daemon
 
