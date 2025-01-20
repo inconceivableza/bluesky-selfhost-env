9bf6f83ba6ca9d08b9f82ec37506a8d5036707ec
David Fraser <davidf@inconceivable.co.za>
2025-01-20 09:09:21 +0000

    Added script to backup all profiles

---
+++ ops-helper/backup-scripts/backup-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+restic_profiles="`resticprofile profiles | grep '^^  .*:' | cut -d: -f1`"
+echo Will backup the following profiles: $restic_profiles
+errors=""
+for p in $restic_profiles
+  do
+    failure resticprofile run-schedule backup@$p || { errors="$errors $p" ; }
+  done
+if [ "$errors" -eq "" ]
+  then
+    echo Successfully backed up the following restic profiles: $restic_profiles
+  else
+    echo Errors backing up the following restic profiles: $errors 2>&1
+  fi
+
