c66240209a26ea46bc350925ded58393dfc5ba97
David Fraser <davidf@inconceivable.co.za>
2025-01-21 10:54:11 +0000

    Initialize remote repos if defined

--- ops-helper/backup-scripts/restic-init.sh
+++ ops-helper/backup-scripts/restic-init.sh
@@ -5,3 +5,18 @@ restic cat config 2>/dev/null || {
   restic init
 }
 
+if [[ "$RESTIC_REMOTE_REPO1" != "" ]]
+  then
+    restic -r $RESTIC_REMOTE_REPO1 init  --from-repo $RESTIC_REPOSITORY --copy-chunker-params
+  fi
+
+if [[ "$RESTIC_REMOTE_REPO2" != "" ]]
+  then
+    restic -r $RESTIC_REMOTE_REPO2 init  --from-repo $RESTIC_REPOSITORY --copy-chunker-params
+  fi
+
+if [[ "$RESTIC_REMOTE_REPO3" != "" ]]
+  then
+    restic -r $RESTIC_REMOTE_REPO3 init  --from-repo $RESTIC_REPOSITORY --copy-chunker-params
+  fi
+
