21ea2bc32d84e96926a9eeba94a7338602492974
David Fraser <davidf@inconceivable.co.za>
2025-01-22 09:18:47 +0000

    Made clean script that actually cleans the same as the restic backups, including postgres

--- ops-helper/backup-scripts/clean.sh
+++ ops-helper/backup-scripts/clean.sh
@@ -1,24 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo This will delete ALL data on this bluesky system. ARE YOU SURE??? Checking in 5 seconds. >&2
 sleep 5
-read -p "Enter DELETE to confirm" -n 1 -r
+read -p "Enter DELETE to confirm: " -r
 echo
+BACKUP_PATHS="/data/bgs/ /data/bsky/ /data/pds/ /data/opensearch/"
+POSTGRES_DBS="bgs bsky carstore ozone palomar plc"
+errors=
 if [[ "$REPLY" == "DELETE" ]]
   then
     echo Deleting data...
     for data_path in $BACKUP_PATHS
       do
         echo removing data from $data_path
-        rm -r $data_path/*
+        rm -r $data_path/* || errors="$errors $data_path"
       done
-    for data_path in $SQLITE_PATHS
+
+    for db in $POSTGRES_DBS
       do
-        echo removing sqlite databases from $data_path
-        find $data_path -name "*.sqlite*" -print0 | -xargs -0 rm -r
+        echo Dropping database $db
+        psql postgres -c "drop database ${db};" || errors="$errors database:$db"
       done
-    echo this script does not yet remove postgres data but the restore cleans it
+
+    if [[ "$errors" != "" ]]
+      then
+        echo Errors occurred processing $errors >&2
+      fi
   else
     echo Confirmation not received. Aborting >&2
   fi
