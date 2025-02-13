817a9497ae06533b7775fb966117e80ed5c770b9
David Fraser <davidf@inconceivable.co.za>
2025-02-13 15:36:57 +0000

    Do renames with git before running semgrep, so git tracks file changes better

--- rebranding/run-rewrite.sh
+++ rebranding/run-rewrite.sh
@@ -64,9 +64,10 @@ python $script_dir/export-brand-images.py $BRAND_IMAGES_DIR/$BRAND_IMAGES_FILE |
     cd "$social_app_dir"
     echo patching social-app in `pwd` with $(basename ${BRAND_CONFIG_DIR})
     git reset --hard
+    python ${script_dir}/apply_files.py --config "$BRAND_CONFIG_DIR"/social-app.yml --env-file "$params_file" --env-file "$BRAND_TMP_ENV_FILE" --git-mv --action rename || { echo error running apply-files >&2 ; exit 1 ; }
     semgrep scan --config ${BRAND_CONFIG_DIR}/social-app.yml -a || { echo error running semgrep >&2 ; exit 1 ; }
     cp google-services.json.example google-services.json
-    python ${script_dir}/apply_files.py --config "$BRAND_CONFIG_DIR"/social-app.yml --env-file "$params_file" --env-file "$BRAND_TMP_ENV_FILE" || { echo error running apply-files >&2 ; exit 1 ; }
+    python ${script_dir}/apply_files.py --config "$BRAND_CONFIG_DIR"/social-app.yml --env-file "$params_file" --env-file "$BRAND_TMP_ENV_FILE" -a copy -a svg-html -a svg-tsx || { echo error running apply-files >&2 ; exit 1 ; }
     echo "app_name=${REBRANDING_NAME}" > branding.env
 ) || { show_error "Patching social-app failed:" "examine above error messages and correct" ; exit 1 ; }
 
