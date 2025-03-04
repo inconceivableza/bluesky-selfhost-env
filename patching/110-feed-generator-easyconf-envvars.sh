e421c7324d32130df56b4b25f62546f607fa47fe
David Fraser <davidf@inconceivable.co.za>
2025-03-04 14:16:57 +0200

    macos sed expects a suffix after the -i (linux sed can do this too, but doesn't require it)

--- patching/110-feed-generator-easyconf-envvars.sh
+++ patching/110-feed-generator-easyconf-envvars.sh
@@ -14,7 +14,7 @@ patch -p1 < ${p_}
 
 f=./src/server.ts
 if [ -f $f ] && [ -n "`grep -R 'https://plc.directory' $f`" ];then
-       sed -i "s#plcUrl: 'https://plc.directory'#plcUrl: process.env.FEEDGEN_PLC_URL ?? 'https://plc.directory'#g" $f
+       sed -i.bak "s#plcUrl: 'https://plc.directory'#plcUrl: process.env.FEEDGEN_PLC_URL ?? 'https://plc.directory'#g" $f
        echo "plcUrl: hardcoded   => process.env.FEEDGEN_PLC_URL ?? hardcoded  for $f"
 fi
 popd
--- patching/121-ozone-support-latest-api.sh
+++ patching/121-ozone-support-latest-api.sh
@@ -6,7 +6,7 @@ echo "rDir:   ${rDir}"
 d_=${rDir}/ozone
 pushd ${d_}
 
-sed -i -E 's#"@atproto/api": "([0-9\.]+)"#"@atproto/api": "^\1"#'     package.json
-sed -i -E 's#"@atproto/ozone": "([0-9\.]+)"#"@atproto/ozone": "^\1"#' service/package.json
+sed -i.bak -E 's#"@atproto/api": "([0-9\.]+)"#"@atproto/api": "^\1"#'     package.json
+sed -i.bak -E 's#"@atproto/ozone": "([0-9\.]+)"#"@atproto/ozone": "^\1"#' service/package.json
 
 popd
