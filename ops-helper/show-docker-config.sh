835ce233260a6c71f6c9708f887e9a01777effe1
David Fraser <davidf@inconceivable.co.za>
2025-07-29 14:40:13 +0200

    Added script to show config and docker compose config

---
+++ ops-helper/show-docker-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+script_path="`realpath "$0"`"
+script_dir="`dirname "$script_path"`/.."
+. "$script_dir/utils.sh"
+
+cd "$script_dir"
+[ -f $params_file ] || { show_error "Params file not found" at $params_file ; exit 1 ; }
+
+set -o allexport
+. "$params_file"
+set +o allexport
+
+show_heading "Showing configuration"
+make echo
+
+show_heading "Showing docker compose config"
+make docker-compose-exec cmd='config'
+
