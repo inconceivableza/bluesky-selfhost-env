aa49de3b045365fbbecca08f042842e5c7d0b1b1
David Fraser <davidf@inconceivable.co.za>
2025-08-05 10:53:16 +0200

    Rather than having a separate config dir for each domain use templated yml files and a branding.yml file for each brand and read the domain-specific config from the environment

--- rebranding/run-rewrite.sh
+++ rebranding/run-rewrite.sh
@@ -3,7 +3,7 @@
 script_path="`realpath "$0"`"
 script_dir="`dirname "$script_path"`"
 BRAND_CONFIG_DIR="$1"
-BRAND_IMAGES_DIR="$2"
+REBRAND_TEMPLATE_DIR="$script_dir"/repo-rules
 . $script_dir/../utils.sh
 
 main_rebranding_dir="$script_dir"
@@ -24,18 +24,14 @@ function show_subdirs_with_brand_images() {
 }
 
 function usage() {
-    echo syntax "$0" brand_config_dir brand_images_dir >&2
-    echo defined brand config dirs may include:
-    echo "  "`show_subdirs_with_brand_config "$main_rebranding_rel"`
-    [ -d "$main_rebranding_rel" ] && echo "  "`show_subdirs_with_brand_config "$alt_rebranding_rel"`
-    echo to create a new one, copy opensky-local-com and adjust for your domain name
-    echo defined brand images dirs may include: `ls */*-branding.svg 2>/dev/null | sed 's#/.*-branding.svg##' | sort -u`
+    echo syntax "$0" brand_config_dir >&2
+    echo defined brand config dirs may include: `ls */*-branding.svg 2>/dev/null | sed 's#/.*-branding.svg##' | sort -u`
     echo "  "`show_subdirs_with_brand_images "$main_rebranding_rel"`
     [ -d "$main_rebranding_rel" ] && echo "  "`show_subdirs_with_brand_images "$alt_rebranding_rel"`
     echo to create a new one, copy opensky and adjust for your images
 }
 
-[[ "$1" == "-h" || "$1" == "--help" || "$BRAND_CONFIG_DIR" == "" || "$BRAND_IMAGES_DIR" == "" ]] && {
+[[ "$1" == "-h" || "$1" == "--help" || "$BRAND_CONFIG_DIR" == "" ]] && {
     usage
     exit 1
 }
@@ -44,28 +40,23 @@ function usage() {
     usage
     exit 1
 }
-[ ! -d "$BRAND_IMAGES_DIR" ] && {
-    show_error "Directory not found" "brand_images_dir: $BRAND_IMAGES_DIR"
-    usage
-    exit 1
-}
 
 BRAND_CONFIG_DIR="`cd "$BRAND_CONFIG_DIR" ; pwd`"
-BRAND_IMAGES_DIR="`cd "$BRAND_IMAGES_DIR" ; pwd`"
 
 cd "$script_dir"
 
 [ -f venv/bin/activate ] && . venv/bin/activate
 
-[ -f "$BRAND_CONFIG_DIR"/social-app.yml ] || {
-    show_error "Brand config not found" "in directory $BRAND_CONFIG_DIR"
+BRAND_IMAGES_FILE="`basename "$BRAND_CONFIG_DIR"`-branding.svg" 
+
+[ -f "$BRAND_CONFIG_DIR/$BRAND_IMAGES_FILE" ] || {
+    show_error "Brand images not found" "in directory $BRAND_CONFIG_DIR under $BRAND_IMAGES_FILE"
     exit 1
 }
 
-BRAND_IMAGES_FILE="`basename "$BRAND_IMAGES_DIR"`-branding.svg" 
-
-[  -f "$BRAND_IMAGES_DIR/$BRAND_IMAGES_FILE" ] || {
-    show_error "Brand images not found" "in directory $BRAND_IMAGES_DIR under $BRAND_IMAGES_FILE"
+[ -f "$BRAND_CONFIG_DIR/branding.yml" ] || {
+    show_error "Branding config not found" "in directory $BRAND_CONFIG_DIR under branding.yml"
+    show_info "Copy and adjust the template" "from $REBRAND_TEMPLATE_DIR/branding.yml.example"
     exit 1
 }
 
@@ -76,7 +67,7 @@ if [ "$os" == "macos" ]
     BRAND_TMP_ENV_FILE="`mktemp --suffix=.env`"
   fi
 echo "BRAND_CONFIG_DIR=${BRAND_CONFIG_DIR}" >> "$BRAND_TMP_ENV_FILE" 
-echo "BRAND_IMAGES_DIR=${BRAND_IMAGES_DIR}" >> "$BRAND_TMP_ENV_FILE" 
+echo "BRAND_IMAGES_DIR=${BRAND_CONFIG_DIR}" >> "$BRAND_TMP_ENV_FILE" 
 
 export MANUAL_REBRANDED_REPOS="$REBRANDED_REPOS"
 [ -f "$params_file" ] && { set -a ; . "$params_file" ; set +a ; }
@@ -87,7 +78,7 @@ function needs_images() {
   show_info "Checking whether images should be rebuilt" "for $REBRANDED_REPOS"
   for branded_repo in $REBRANDED_REPOS
     do
-      if yq -e 'has("rename_paths") or has("copy_files") or has("svg_html_subst") or has("svg_tsx_subst")' ${BRAND_CONFIG_DIR}/${branded_repo}.yml > /dev/null 2>&1
+      if yq -e 'has("rename_paths") or has("copy_files") or has("svg_html_subst") or has("svg_tsx_subst")' ${REBRAND_TEMPLATE_DIR}/${branded_repo}.mustache.yml > /dev/null 2>&1
         then
           show_info "Image build required" "for $branded_repo"
           return 0
@@ -98,11 +89,14 @@ function needs_images() {
 
 if needs_images
   then
-    python $script_dir/export-brand-images.py $BRAND_IMAGES_DIR/$BRAND_IMAGES_FILE || { show_error "Error exporting images" "from $BRAND_IMAGES_DIR" ; exit 1 ; }
+    python $script_dir/export-brand-images.py $BRAND_CONFIG_DIR/$BRAND_IMAGES_FILE || { show_error "Error exporting images" "from $BRAND_CONFIG_DIR" ; exit 1 ; }
   fi
 
 for branded_repo in $REBRANDED_REPOS
   do
+    # generate the branding rules from the template
+    [ -f ${REBRAND_TEMPLATE_DIR}/${branded_repo}.mustache.yml ] || { echo could not find ${branded_repo}.mustache.yml in ${REBRAND_TEMPLATE_DIR} >&2 ; exit 1 ; }
+    python ${script_dir}/generate_rules.py -c ${BRAND_CONFIG_DIR}/branding.yml -e ${params_file} ${REBRAND_TEMPLATE_DIR}/${branded_repo}.mustache.yml ${BRAND_CONFIG_DIR}/${branded_repo}.yml
     [ -f ${BRAND_CONFIG_DIR}/${branded_repo}.yml ] || { echo could not find ${BRAND_CONFIG_DIR}/${branded_repo}.yml >&2 ; exit 1 ; }
     repo_dir="$script_dir/../repos/${branded_repo}"
     [ -d "$repo_dir" ] || { echo could not find dir for $repo_dir - place at $repo_dir  >&2 ; exit 1 ; }
