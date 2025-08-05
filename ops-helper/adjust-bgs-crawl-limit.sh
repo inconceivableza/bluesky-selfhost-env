d59e71261de65fe57b3a6ccebc5a257d8bf4bdef
David Fraser <davidf@inconceivable.co.za>
2025-08-05 12:04:06 +0200

    Make this use the proper environment rather than hardcoding things

--- ops-helper/adjust-bgs-crawl-limit.sh
+++ ops-helper/adjust-bgs-crawl-limit.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-. config/secrets-passwords.env
-curl  -L https://bgs.foodios.vabl.dev/admin/subs/perDayLimit -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
-curl  -L -X POST https://bgs.foodios.vabl.dev/admin/subs/setPerDayLimit?limit=500 -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
+
+script_path="`realpath "$0"`"
+script_dir="`dirname "$script_path"`/.."
+. "$script_dir/utils.sh"
+source_env
+
+. "$script_dir/config/secrets-passwords.env"
+
+[ "$bgsFQDN" == "" ] && { echo "Couldn't find bgs domain from bgsFQDN environment; please check $params_file" >&2 ; exit 1 ; }
+[ "$BGS_ADMIN_KEY" == "" ] && { echo "Couldn't find bgs admin key from environment or secrets file; please check BGS_ADMIN_KEY in $params_file and $script_dir/config/secrets-passwords.env" >&2 ; exit 1 ; }
+
+curl  -L https://$bgsFQDN/admin/subs/perDayLimit -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
+curl  -L -X POST https://$bgsFQDN/admin/subs/setPerDayLimit?limit=500 -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
