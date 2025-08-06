5b6d14c3898267d1c5afb2461ec01db70474653e
David Fraser <davidfraser@users.noreply.github.com>
2025-08-06 16:34:18 +0200

    Abandon package attempts and go with individual modules

--- selfhost_scripts/setup.py
+++ selfhost_scripts/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="selfhost_scripts",
     version="0.1.0",
-    packages=find_packages(),
+    py_modules=["env_utils", "secret_types", "gen_secrets"],
     python_requires=">=3.6",
     install_requires=[
         "PyYAML",
