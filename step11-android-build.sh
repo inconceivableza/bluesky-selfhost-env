635ab9aec3ea75f959c948e3df9dc0b7b7873d25
David Fraser <davidf@inconceivable.co.za>
2025-08-27 16:35:48 +0200

    Set build profile as selected, and use that to determine filenames Build into temporary directory before moving Also determine programatically whether build will be an apk or aab No more guessing build filenames...

--- step11-android-build.sh
+++ step11-android-build.sh
@@ -12,7 +12,7 @@ if [ "$#" -eq 1 ]
 . "$script_dir/utils.sh"
 source_env || exit 1
 
-show_heading "Building Android app" "using applied branding and domain name changes"
+show_heading "Building Android app" "using applied branding and ${build_profile} profile"
 
 if [ "$JAVA_HOME" == "" ] || [ ! -d "$JAVA_HOME" ]
   then
@@ -37,35 +37,82 @@ cd "$script_dir/repos/social-app"
 
 which nvm || . ~/.nvm/nvm.sh
 nvm use 20
+
+function get_expected_extension() {
+  # based on https://docs.expo.dev/build-reference/apk/
+  devClient="`jq -r .build.${build_profile}.developmentClient eas.json`"
+  dist="`jq -r .build.${build_profile}.distribution eas.json`"
+  buildType="`jq -r .build.${build_profile}.android.buildType eas.json`"
+  gradCmd="`jq -r .build.${build_profile}.android.gradleCommand eas.json`"
+  [ "$buildType" != "" ] && [ "$buildType" != null ] && { echo $buildType ; return 0; }
+  [ "$gradCmd" != "" ] && [ "$gradCmd" != null ] && { show_warning "Cannot determine extension" "profile $build_profile has gradle command configured" ; return 1 ; }
+  if [ "$dist" == "internal" ] || [ "$devClient" == "true" ]
+    then
+      echo apk
+      return 0
+    fi
+  echo aab
+}
+
+show_heading "Determining build info" "to set filename etc"
+build_flavour=${build_profile}
+build_name="`jq -r .name package.json`"
+build_version="`jq -r .version package.json`"
+build_date="`date +%Y%m%d`"
+build_timestamp="`date +%H%M%S`"
+build_dir="$script_dir/tmp-builds/android-${build_name}-${build_version}-${build_date}-${build_timestamp}"
+build_id="${build_name}-${build_version}-${build_flavour}-${build_date}-${build_timestamp}"
+target_ext=`get_expected_extension`
+[ "$target_ext" == "" ] && exit 1
+target_dir="$script_dir/android-builds/"
+build_file="$build_id.$target_ext"
+show_info --oneline "Expected build" "${build_file}"
+show_info --oneline "Creating temporary build directory" "at ${build_dir}"
+mkdir -p "$build_dir"
+mkdir -p "$target_dir"
+
+show_heading "Running yarn" "to ensure everything is installed"
 yarn
 
-if npx eas-cli build -p android --local
+show_heading "Running build" "for profile $build_profile to generate $build_file"
+if npx eas-cli build -p android --local -e $build_profile --output="$build_dir/$build_file"
   then
-    build_file="`ls --sort=time --time=mtime | grep "build-[0-9]*.aab" | head -n 1`"
-    build_number="`echo "$build_file" | sed 's/build-\([0-9]*\).aab/\1/'`"
-    build_name="`jq -r .name package.json`"
-    build_version="`jq -r .version package.json`"
-    build_date="`date +%Y-%m-%d`"
-    build_id="${build_name}-${build_version}-${build_date}-${build_number}"
-    show_heading "Build complete:" "presumed aab file is:"
-    ls -l "$build_file"
-    app_name="${REBRANDING_NAME:=bluesky}"
-    [ -f ${app_name}.apks ] && {
-      backup_file=${app_name}-"$(stat -t --format="%y" ${app_name}.apks | cut -c 1-16 | sed 's/[- :]//g')".apks
-      show_warning "Renaming intermediate file" "${app_name}.apks -> ${backup_file}"
-      mv ${app_name}.apks ${backup_file}
-      ls -l ${backup_file}
-    }
-    show_info "Extracting apk" "and renaming to ${app_name}"
-    bundletool build-apks --bundle $build_file --output=${app_name}.apks --mode=universal
-    ls -l ${app_name}.apks
-    unzip ${app_name}.apks universal.apk
-    rm ${app_name}.apks
-    mv universal.apk ${build_id}.apk
-    touch -r $build_file ${build_id}.apk
-    mv ${build_file} ${build_id}.aab
-    show_info "Android app extracted"
-    ls -l ${build_id}.apk ${build_id}.aab
+    cd "$build_dir"
+    if [ -f "$build_file" ]
+      then
+        show_heading "Build complete:" "output file is:"
+        ls -l "$build_file"
+      else
+        show_error "Build complete but no build found" "in $build_dir/$build_file"
+        exit 1
+      fi
+    if [ "$target_ext" == "aab" ]
+      then
+        app_name="${REBRANDING_NAME:=bluesky}"
+        [ -f ${app_name}.apks ] && {
+          backup_file=${app_name}-"$(stat -t --format="%y" ${app_name}.apks | cut -c 1-16 | sed 's/[- :]//g')".apks
+          show_warning "Renaming intermediate file" "${app_name}.apks -> ${backup_file}"
+          mv ${app_name}.apks ${backup_file}
+          ls -l ${backup_file}
+        }
+        show_info "Extracting apk" "and renaming to ${app_name}"
+        echo bundletool build-apks --bundle $build_file --output=${app_name}.apks --mode=universal
+        bundletool build-apks --bundle $build_file --output=${app_name}.apks --mode=universal || { show_error "bundletool failing" "dumping env" ; export ; }
+        ls -l ${app_name}.apks
+        unzip ${app_name}.apks universal.apk
+        rm ${app_name}.apks
+        mv universal.apk ${target_dir}/${build_id}.apk
+        touch -r $build_file ${target_dir}/${build_id}.apk
+        mv ${build_file} ${target_dir}/${build_id}.aab
+      else
+        show_info "Moving build file" "${build_file} to ${build_dir}"
+        mv ${build_file} ${target_dir}/${build_file}
+      fi
+    show_info "Cleaning up tmp build dir" "${build_dir}"
+    cd "${target_dir}"
+    rmdir "${build_dir}"
+    show_info "Android app build complete" "and available in ${target_dir}"
+    ls -l ${build_id}.*
   else
     show_error "Error building Android app:" "see above for details"
     exit 1
--- step12-ios-build.sh
+++ step12-ios-build.sh
@@ -12,33 +12,55 @@ if [ "$#" -eq 1 ]
 . "$script_dir/utils.sh"
 source_env || exit 1
 
-show_heading "Building iOS app" "using applied branding and domain name changes"
+show_heading "Building iOS app" "using applied branding and ${build_profile} profile"
 
 cd "$script_dir/repos/social-app"
 
 which nvm || . ~/.nvm/nvm.sh
 nvm use 20
+
+show_heading "Determining build info" "to set filename etc"
+build_flavour=${build_profile}
+build_name="`jq -r .name package.json`"
+build_version="`jq -r .version package.json`"
+build_date="`date +%Y%m%d`"
+build_timestamp="`date +%H%M%S`"
+build_dir="$script_dir/tmp-builds/ios-${build_name}-${build_version}-${build_date}-${build_timestamp}"
+build_id="${build_name}-${build_version}-${build_flavour}-${build_date}-${build_timestamp}"
+build_file="$build_id.ipa"
+target_dir="$script_dir/ios-builds/"
+show_info --oneline "Expected build" "${build_file}"
+show_info --oneline "Creating temporary build directory" "at ${build_dir}"
+mkdir -p "$build_dir"
+mkdir -p "$target_dir"
+
+show_heading "Running yarn" "to ensure everything is installed"
 yarn
 
 # issues with versions (ran ~npx expo install --check`)
+show_heading "Running expo install check" "to check everything is installed"
 npx expo install --check
 
 # issues with npx doctor (added things to package.json, but was this necessary?)
 
-
-
-if npx eas-cli build -p ios --local -e preview
+show_heading "Running build" "for profile $build_profile to generate $build_file"
+if npx eas-cli build -p ios --local -e ${build_profile} --output="$build_dir"
   then
-    build_file="`ls -t | grep "build-[0-9]*.ipa" | head -n 1`"
-    build_number="`echo "$build_file" | sed 's/build-\([0-9]*\).ipa/\1/'`"
-    build_name="`jq -r .name package.json`"
-    build_version="`jq -r .version package.json`"
-    build_date="`date +%Y-%m-%d`"
-    build_id="${build_name}-${build_version}-${build_date}-${build_number}"
-    show_heading "Build complete:" "presumed ipa file is:"
+    cd "$build_dir"
+    if [ -f "$build_file" ]
+      then
+        show_heading "Build complete:" "output file is:"
+        ls -l "$build_file"
+      else
+        show_error "Build complete but no build found" "in $build_dir/$build_file"
+        exit 1
+      fi
     ls -l "$build_file"
-    mv ${build_file} ${build_id}.ipa
-    show_info "iOS app renamed"
+    mv ${build_file} ${target_dir}/${build_id}.ipa
+    show_info "Cleaning up tmp build dir" "${build_dir}"
+    cd "${target_dir}"
+    rmdir "${build_dir}"
+    show_info "iOS app build complete" "and available in ${target_dir}"
     ls -l ${build_id}.ipa
   else
     show_error "Error building iOS app:" "see above for details"
