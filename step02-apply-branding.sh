94a35ca19f51fdd48da05fcabfb06e70fadfaaa8
David Fraser <davidf@inconceivable.co.za>
2025-09-01 16:57:46 +0200

    Don't automatically rebrand a repository just because given on the commandline This allows this script to be used for the other branching steps for specific repositories

--- step02-apply-branding.sh
+++ step02-apply-branding.sh
@@ -18,7 +18,9 @@ if [ $# -gt 0 ]
     cmdlineDirs="$@"
     show_info "Will only run on repos" $cmdlineDirs
     repoDirs="$(for cmdlineDir in ${cmdlineDirs}; do echo $script_dir/repos/$cmdlineDir ; done)"
-    REBRANDED_REPOS="$cmdlineDirs"
+    TARGET_REPOS="$cmdlineDirs"
+  else
+    TARGET_REPOS="$REBRANDED_REPOS"
   fi
 
 show_heading "Auto-creating latest work branch" "from the different repositories"
@@ -30,7 +32,7 @@ for repoDir in $repoDirs
       repo_branch_varname=$(basename $repoDir | sed 's/-/_/g')_branch
       src_branch=${!repo_branch_varname:-origin/main}
       [[ "$src_branch" != "origin/main" ]] && show_info "Using branch $src_branch" from \$$repo_branch_varname for repo $(basename $repoDir)
-      $script_dir/autobranch.sh work $src_branch dockerbuild local-rebranding-$REBRANDING_NAME $src_branch main
+      true # $script_dir/autobranch.sh work $src_branch dockerbuild local-rebranding-$REBRANDING_NAME $src_branch main
     ) || { show_error "Error updating to latest branch:" "inspect $repoDir and adjust as necessary" ; exit 1 ; }
   done
 
@@ -51,35 +53,41 @@ if [ "$REBRANDING_DISABLED" == "true" ]
       echo "https://github.com/bluesky-social/social-app?tab=readme-ov-file#forking-guidelines"
       exit 1
     fi
-    
-    show_heading "Rebranding repos: $REBRANDED_REPOS" "by scripted changes"
-    REBRANDING_SCRIPT_ABS="`realpath "$REBRANDING_SCRIPT"`"
-    [ "$REBRANDING_NAME" == "" ] && { show_error "Brand name undefined:" "please set REBRANDING_NAME in $params_file" ; exit 1 ; }
-    for branded_repo in $REBRANDED_REPOS
-      do
-        (
-          cd "$script_dir/repos/$branded_repo"
-          show_info "Branching $branded_repo" "before applying branding changes"
-          $script_dir/autobranch.sh -C local-rebranding-$REBRANDING_NAME work dockerbuild
-        )
-      done
-    show_info "Rebranding for $REBRANDING_NAME" "by scripted changes"
-    "$REBRANDING_SCRIPT_ABS" "$REBRANDING_DIR" || { show_error "Rebranding script error:" "Please examine and correct before continuing; ran $REBRANDING_SCRIPT_ABS \"$REBRANDING_DIR\"" ; exit 1 ; }
-    for branded_repo in $REBRANDED_REPOS
-      do
-        (
-          cd "$script_dir/repos/$branded_repo"
-          git diff --exit-code --stat && { show_warning "no changes to $branded_repo:" "check if branding config is correct" ; exit 0 ; }
-          show_info "Committing branding changes" "to $branded_repo"
-          git commit -a -m "Automatic rebranding to $REBRANDING_NAME" || {
-            show_error "Error auto-committing branding changes:" "retrying with git hooks disabled, but you may want to review the above errors"
-            git commit --no-verify -a -m "Automatic rebranding to $REBRANDING_NAME (hooks disabled due to error)" || {
-              show_error "Error auto-committing branding changes:" "even when git hooks disabled; please correct in $repoDir"
-              exit 1
-            }
-          }
-        )
-      done
+    REBRANDED_TARGET_REPOS="$(for f in $REBRANDED_REPOS; do [ "${TARGET_REPOS/$f/}" != "${TARGET_REPOS}" ] && echo $f ; done)"
+    if [ "$REBRANDED_TARGET_REPOS" != "" ]
+      then
+        show_heading "Rebranding repos: $REBRANDED_TARGET_REPOS" "by scripted changes"
+        exit 1
+        REBRANDING_SCRIPT_ABS="`realpath "$REBRANDING_SCRIPT"`"
+        [ "$REBRANDING_NAME" == "" ] && { show_error "Brand name undefined:" "please set REBRANDING_NAME in $params_file" ; exit 1 ; }
+        for branded_repo in $REBRANDED_TARGET_REPOS
+          do
+            (
+              cd "$script_dir/repos/$branded_repo"
+              show_info "Branching $branded_repo" "before applying branding changes"
+              $script_dir/autobranch.sh -C local-rebranding-$REBRANDING_NAME work dockerbuild
+            )
+          done
+        show_info "Rebranding for $REBRANDING_NAME" "by scripted changes"
+        "$REBRANDING_SCRIPT_ABS" "$REBRANDING_DIR" || { show_error "Rebranding script error:" "Please examine and correct before continuing; ran $REBRANDING_SCRIPT_ABS \"$REBRANDING_DIR\"" ; exit 1 ; }
+        for branded_repo in $REBRANDED_TARGET_REPOS
+          do
+            (
+              cd "$script_dir/repos/$branded_repo"
+              git diff --exit-code --stat && { show_warning "no changes to $branded_repo:" "check if branding config is correct" ; exit 0 ; }
+              show_info "Committing branding changes" "to $branded_repo"
+              git commit -a -m "Automatic rebranding to $REBRANDING_NAME" || {
+                show_error "Error auto-committing branding changes:" "retrying with git hooks disabled, but you may want to review the above errors"
+                git commit --no-verify -a -m "Automatic rebranding to $REBRANDING_NAME (hooks disabled due to error)" || {
+                  show_error "Error auto-committing branding changes:" "even when git hooks disabled; please correct in $repoDir"
+                  exit 1
+                }
+              }
+            )
+          done
+      else
+        show_warning "Not rebranding" "as none of the target repositories are in REBRANDED_REPOS"
+    fi
   fi
 
 show_heading "Deleting existing dockerbuild branches" "from the different repositories"
