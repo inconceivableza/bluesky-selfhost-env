ddd15354cd90173254f2426e45ff0f0e44754ee0
David Fraser <davidf@inconceivable.co.za>
2025-09-02 14:16:09 +0200

    Some help in installing certificates on mobile browsers for testing Actually this is trickier than it looks Sometimes the file browser doesn't show the files till a restart And also you need to get DNS working...

--- step21-setup-certs.sh
+++ step21-setup-certs.sh
@@ -12,6 +12,24 @@ if [ "$EMAIL4CERTS" == "internal" ]
     make getCAcert
     [ "$NOINSTALLCERTS" != "true" ] && make installCAcert certCollections
 
+    if [ "$1" == "--android" ]
+      then
+        show_heading "Installing to Android" "for mobile testing"
+        certs="root.crt intermediate.crt"
+        (
+          cd $script_dir/certs
+          certs_dir=Download/certs-$DOMAIN
+          adb shell mkdir /sdcard/$certs_dir
+          adb push $certs /sdcard/$certs_dir
+          show_info "Manually install certificates" "on Android device"
+          adb shell am start -a android.intent.action.VIEW -d content://com.android.externalstorage.documents/root/primary%3ADownload
+          # adb shell am start -n com.android.certinstaller/.CertInstallerMain -a android.intent.action.VIEW -t application/x-x509-ca-cert -d file:///sdcard/Download/
+        )
+      else
+        show_heading --oneline "For mobile testing" "you will need to install the certificates into the mobile devices"
+        show_info --oneline "Run this script" "with --android to install into local android device or emulator via adb"
+        echo
+      fi
     show_heading "Don't forget to install the certificate" "in $script_dir/certs/root.crt into your local web browser"
   else
     show_heading "Certificates will be auto-generated" "using let's encrypt, with $EMAIL4CERTS as the email address"
