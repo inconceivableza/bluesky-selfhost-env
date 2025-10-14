0a177888012ba99f43354f4d7c2fc036e3916ddf
David Fraser <davidf@inconceivable.co.za>
2025-10-14 13:33:34 +0200

    Always restart bsky last

--- do-restart.sh
+++ do-restart.sh
@@ -5,9 +5,23 @@ script_dir="`dirname "$script_path"`"
 . "$script_dir/utils.sh"
 
 [[ "$1" == "" ]] && { echo Syntax $0 service... >&2 ; exit 1 ; }
+services="$(echo "$@" | sed 's/ /\n/g')"
+restart_bsky=
+[[ "$(echo "$services" | grep '^bsky$')" == "bsky" ]] && {
+  show_info "Bsky service selected" "will restart after other services"
+  restart_bsky=1
+  services="$(echo "$services" | grep -v '^bsky$')"
+}
+services="$(echo $services)"
 
 show_heading "Running docker down" "$*"
 make docker-compose-exec cmd="down $*"
-show_heading "Running docker up -d" "$*"
-make docker-compose-exec cmd="up -d $*"
+[ "$services" != "" ] && {
+  show_heading "Running docker up -d" $services
+  make docker-compose-exec cmd="up -d $services"
+}
+[ "$restart_bsky" == "1" ] && {
+  show_heading "Running docker up -d" bsky
+  make docker-compose-exec cmd="up -d bsky"
+}
 
