f6e11f49ca95d746a7e6d4c8441ce2aa7eaacdce
David Fraser <davidf@inconceivable.co.za>
2025-10-29 10:03:13 +0200

    Helper script for running social-app debug mode in a way that can launch android emulator easily

---
+++ debug-social-app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+script_path="`realpath "$0"`"
+script_dir="`dirname "$script_path"`"
+. "$script_dir/utils.sh"
+
+show_heading "Running yarn in debug mode" "for social-app"
+
+show_info --oneline "Checking Android home" "for launching in emulator"
+check_android_home
+
+cd "$script_dir/repos/social-app"
+
+show_info --oneline "Running yarn install"
+yarn
+show_info --oneline "Running yarn start"
+yarn start
+
