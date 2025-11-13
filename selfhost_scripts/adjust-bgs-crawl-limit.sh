edb669b2c1ac72dcb339c5c0491bbfe76e809b74
David Fraser <davidf@inconceivable.co.za>
2025-11-13 09:45:27 +0200

    Use wget instead of curl: curl is complaining with haproxy PROXY Only source the bgs-secrets and ensure they're up to date

--- selfhost_scripts/adjust-bgs-crawl-limit.sh
+++ selfhost_scripts/adjust-bgs-crawl-limit.sh
@@ -5,7 +5,9 @@ script_dir="$(cd `dirname "$script_path"`/.. ; pwd)"
 . "$script_dir/utils.sh"
 source_env
 
-. "$script_dir/config/secrets-passwords.env"
+BGS_SECRETS_FILE="$script_dir/config/bgs-secrets.env"
+make $BGS_SECRETS_FILE
+. $BGS_SECRETS_FILE
 
 [ "$bgsFQDN" == "" ] && { show_warning "Couldn't find bgs domain" "from bgsFQDN environment; please check $params_file" >&2 ; exit 1 ; }
 [ "$BGS_ADMIN_KEY" == "" ] && { show_warning "Couldn't find bgs admin key" "from environment or secrets file; please check BGS_ADMIN_KEY in $params_file and $script_dir/config/secrets-passwords.env" >&2 ; exit 1 ; }
@@ -16,17 +18,17 @@ source_env
 }
 [ "$BGS_LIMIT" == "" ] && BGS_LIMIT=500
 
-previous_limit="$(curl -s -L https://$bgsFQDN/admin/subs/perDayLimit -H "Authorization: Bearer ${BGS_ADMIN_KEY}" | jq -r .limit)"
+previous_limit="$(wget -qO- --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/perDayLimit"| jq -r .limit)"
 if [ "$previous_limit" == "$BGS_LIMIT" ]
   then
     show_info --oneline "Relay crawl limit" "already set to $BGS_LIMIT; not adjusting"
   else
-    curl  -L -X POST https://$bgsFQDN/admin/subs/setPerDayLimit?limit=500 -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
-    new_limit="$(curl -s -L https://$bgsFQDN/admin/subs/perDayLimit -H "Authorization: Bearer ${BGS_ADMIN_KEY}" | jq -r .limit)"
+    wget --method=POST --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/setPerDayLimit?limit=500"
+    new_limit="$(wget -qO- --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/perDayLimit" | jq -r .limit)"
     [ "$new_limit" == "" ] && {
       show_error "Error setting new limit:" "will show debug output from querying it"
       show_info "bgs admin key length" "$(echo -n ${BGS_ADMIN_KEY} | wc -c)"
-      curl -v -L https://$bgsFQDN/admin/subs/perDayLimit -H "Authorization: Bearer ${BGS_ADMIN_KEY}"
+      wget -vO- --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/perDayLimit"
       exit 1
     }
     show_info --oneline "Relay crawl limit" "was $previous_limit, adjusted to $new_limit"
