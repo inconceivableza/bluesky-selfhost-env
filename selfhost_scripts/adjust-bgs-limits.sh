d08bd2798eacadd8b5f3126f6dc54dd45b8c6d01
David Fraser <davidf@inconceivable.co.za>
2025-11-18 16:25:16 +0200

    Support setting all the limits and clean this up (some limits only settable through env vars)

--- selfhost_scripts/adjust-bgs-limits.sh
+++ selfhost_scripts/adjust-bgs-limits.sh
@@ -23,7 +23,11 @@ make $BGS_SECRETS_FILE
    BGS_PDS_REPO_LIMIT="$1"
    shift 1
 }
-[ "$BGS_PDS_REPO_LIMIT" == "" ] && BGS_PDS_REPO_LIMIT=100000
+[ "$BGS_PDS_REPO_LIMIT" == "" ] && BGS_PDS_REPO_LIMIT=1000000
+[ "$BGS_PDS_CRAWL_RATE_LIMIT" == "" ] && BGS_PDS_CRAWL_RATE_LIMIT=10
+[ "$BGS_PDS_PER_SECOND_LIMIT" == "" ] && BGS_PDS_PER_SECOND_LIMIT=100
+[ "$BGS_PDS_PER_HOUR_LIMIT" == "" ] && BGS_PDS_PER_HOUR_LIMIT=1000000
+[ "$BGS_PDS_PER_DAY_LIMIT" == "" ] && BGS_PDS_PER_DAY_LIMIT=1000000
 
 error=
 
@@ -32,7 +36,7 @@ if [ "$previous_limit" == "$BGS_SUBS_PER_DAY" ]
   then
     show_info --oneline "Relay crawl limit" "already set to $BGS_SUBS_PER_DAY; not adjusting"
   else
-    wget --method=POST --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/setPerDayLimit?limit=$BGS_SUBS_PER_DAY"
+    wget -qO- --method=POST --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/setPerDayLimit?limit=$BGS_SUBS_PER_DAY"
     new_limit="$(wget -qO- --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/subs/perDayLimit" | jq -r .limit)"
     if [ "$new_limit" == "" ]; then
       show_error "Error setting new limit:" "will show debug output from querying it"
@@ -49,12 +53,17 @@ function get_pds_limits() {
 }
 
 previous_limits="$(get_pds_limits)"
-previous_limit="$(echo "$previous_limits" | jq -r '.RepoLimit')"
-if [ "$previous_limit" == "$BGS_PDS_REPO_LIMIT" ]
+previous_repo_limit="$(echo "$previous_limits" | jq -r '.RepoLimit')"
+previous_second_limit="$(echo "$previous_limits" | jq -r '.RateLimit')"
+previous_hour_limit="$(echo "$previous_limits" | jq -r '.HourlyEventLimit')"
+previous_day_limit="$(echo "$previous_limits" | jq -r '.DailyEventLimit')"
+previous_crawl_limit="$(echo "$previous_limits" | jq -r '.CrawlRateLimit')"
+if [[ "$previous_repo_limit" == "$BGS_PDS_REPO_LIMIT" && "$previous_second_limit" == "$BGS_PDS_PER_SECOND_LIMIT" && "$previous_hour_limit" == "$BGS_PDS_PER_HOUR_LIMIT" && "$previous_day_limit" == "$BGS_PDS_PER_DAY_LIMIT" && "$previous_crawl_limit" == "$BGS_PDS_CRAWL_RATE_LIMIT" ]]
   then
-    show_info --oneline "PDS repo limit" "already set to $BGS_PDS_REPO_LIMIT; not adjusting"
+    show_info --oneline "PDS limits" "already set to configured values; not adjusting"
   else
-    adjusted_limits="$(echo "$previous_limits" | jq --arg limit "$BGS_PDS_REPO_LIMIT" '.RepoLimit = ($limit | tonumber)')"
+    adjusted_limits="$(echo "$previous_limits" | jq --arg repo_limit "$BGS_PDS_REPO_LIMIT" --arg second_limit "$BGS_PDS_PER_SECOND_LIMIT" --arg hour_limit "$BGS_PDS_PER_HOUR_LIMIT" --arg day_limit "$BGS_PDS_PER_DAY_LIMIT" --arg crawl_limit "$BGS_PDS_CRAWL_RATE_LIMIT" \
+            '.RepoLimit = ($repo_limit | tonumber) | .RateLimit = ($second_limit | tonumber) | .HourlyEventLimit = ($hour_limit | tonumber) | .DailyEventLimit = ($day_limit | tonumber) | .CrawlRateLimit = ($crawl_limit | tonumber)')"
     post_limits="$(echo "$adjusted_limits" | jq --arg host "$pdsFQDN" '{per_second: .RateLimit, crawl_rate: .CrawlRateLimit, per_hour: .HourlyEventLimit, per_day: .DailyEventLimit, repo_limit: .RepoLimit, host: $host}')"
     post_results="$(wget -qO- --method=POST --body-data="$post_limits" --header="Content-Type: application/json" --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/pds/changeLimits")"
     post_success="$(echo "$post_results" | jq -r .success)"
@@ -64,14 +73,22 @@ if [ "$previous_limit" == "$BGS_PDS_REPO_LIMIT" ]
       show_warning --oneline "bgs limit change error" "${post_results}"
     fi
     new_limits="$(get_pds_limits)"
-    new_limit="$(echo "$new_limits" | jq -r '.RepoLimit')"
-    if [[ "$new_limit" == "" || "$post_success" != "true" ]]; then
-      show_error "Error setting new limit:" "will show debug output from querying it"
+    new_repo_limit="$(echo "$new_limits" | jq -r '.RepoLimit')"
+    new_second_limit="$(echo "$new_limits" | jq -r '.RateLimit')"
+    new_hour_limit="$(echo "$new_limits" | jq -r '.HourlyEventLimit')"
+    new_day_limit="$(echo "$new_limits" | jq -r '.DailyEventLimit')"
+    new_crawl_limit="$(echo "$new_limits" | jq -r '.CrawlRateLimit')"
+    if [[ "$new_repo_limit" == "" || "$post_success" != "true" ]]; then
+      show_error "Error setting new limits:" "will show debug output from querying it"
       show_info "bgs admin key length" "$(echo -n ${BGS_ADMIN_KEY} | wc -c)"
       wget -qO- --header="Authorization: Bearer ${BGS_ADMIN_KEY}" "https://$bgsFQDN/admin/pds/list" | jq
       error=1
     else
-      show_info --oneline "PDS repo limit" "was $previous_limit, now is $new_limit"
+      show_info --oneline "PDS repo limit" "was $previous_repo_limit, now is $new_repo_limit"
+      show_info --oneline "PDS per second limit" "was $previous_second_limit, now is $new_second_limit"
+      show_info --oneline "PDS per hour limit" "was $previous_hour_limit, now is $new_hour_limit"
+      show_info --oneline "PDS per day limit" "was $previous_day_limit, now is $new_day_limit"
+      show_info --oneline "PDS crawl rate limit" "was $previous_crawl_limit, now is $new_crawl_limit"
     fi
   fi
 
