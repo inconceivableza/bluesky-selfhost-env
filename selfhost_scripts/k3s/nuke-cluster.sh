cdfe84ef631527133cdcb6430f13f477f8490902
NevilleVH <1nevillevh@gmail.com>
2025-12-16 10:34:08 +0200

    Fix path to values files

--- selfhost_scripts/k3s/nuke-cluster.sh
+++ selfhost_scripts/k3s/nuke-cluster.sh
@@ -2,4 +2,4 @@
 kubectl delete namespace foodios
 BASE_DIR=$(git rev-parse --show-toplevel)
 $BASE_DIR/selfhost_scripts/k3s/create-all-secrets.sh
-helm install foodios $BASE_DIR/foodios-chart -n foodios --create-namespace -f $BASE_DIR/foodios-chart/values-local.yaml
+helm install foodios $BASE_DIR/foodios-chart -n foodios --create-namespace -f $BASE_DIR/foodios-chart/values/local.yaml
