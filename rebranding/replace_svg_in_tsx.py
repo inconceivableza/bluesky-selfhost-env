c9397e9dadff793583463752b8c3784771e11aad
David Fraser <davidf@inconceivable.co.za>
2025-12-18 14:33:07 +0200

    Do case-insensitive sort in tsx files so we don't get prettier complaining

--- rebranding/replace_svg_in_tsx.py
+++ rebranding/replace_svg_in_tsx.py
@@ -37,6 +37,9 @@ def tagname_to_tsx(tagname):
         return 'TSpan'
     return tagname[:1].upper() + tagname[1:]
 
+def ts_sort(keys):
+    return sorted(dict.fromkeys(keys), key=lambda key: key.lower())
+
 def svg_to_tsx(src):
     # this could be done with more finesse
     tags = set(tag_initial.findall(src))
@@ -59,7 +62,7 @@ def svg_to_tsx(src):
     for xml_attr in set(xml_attr_re.findall(src)):
         new_xml_attr = 'xml' + xml_attr[xap:xap+1].upper() + xml_attr[xap+1:]
         src = src.replace(xml_attr, new_xml_attr)
-    return src, list(sorted(dict.fromkeys(new_tagnames)))
+    return src, list(ts_sort(new_tagnames))
 
 def adjust_svg_and_import(src, svg, config_file=replace_svg_contents_config):
     svg_inner = get_svg_inner(svg)
@@ -81,7 +84,7 @@ def adjust_svg_and_import(src, svg, config_file=replace_svg_contents_config):
         import_replacements[imported_tags] = ', '.join(used_tags)
         imported_svg_objects.extend(tag.strip() for tag in used_tags if tag.strip())
     tsx_tags = [tag for tag in tsx_tags if tag not in imported_svg_objects]
-    new_tags_str = ', '.join(sorted(dict.fromkeys(tsx_tags)))
+    new_tags_str = ', '.join(ts_sort(tsx_tags))
     for import_original, import_new in import_replacements.items():
         result = result.replace(import_original, import_new)
     result = result.replace('@SVG_INNER@', tsx_svg_inner).replace('@SVG_TAGS@', new_tags_str)
