0366c7198bf5dcf6c651006ae63021d822708a75
David Fraser <davidf@inconceivable.co.za>
2025-12-22 09:59:03 +0200

    Don't use the tag variable from .env for build if it contains a git commit hash Add note as this is worth considering when making and using local builds

--- step10-docker-build.sh
+++ step10-docker-build.sh
@@ -48,9 +48,43 @@ make cloneAll
 
 $script_dir/generate-env-files.sh
 
+function get_image_tag_varname() {
+  service="$1"
+  image_tag_def="$(yq -r ".services.${service}.image" $script_dir/docker-compose.yaml 2>/dev/null)"
+  image_tag_varname="$(echo -n "$image_tag_def" | sed 's/^.*:[$][{]\([A-Z_]*IMAGE_TAG\).*$/\1/')"
+  [ "$image_tag_varname" != "$image_tag_def" ] && echo -n "$image_tag_varname"
+}
+
+function is_git_commit() {
+  image_tag_varname="$1"
+  current_tag="${!image_tag_varname}"
+  [ "$(echo -n "$current_tag" | wc -c)" == 40 ] && return 1
+  return 0
+}
+
+function make_image_tag() {
+  repodir="$1"
+  cd $repodir
+  git_commit=$(git log -1 --format=%H)
+  hostname=$(hostname -s)
+  image_tag="$git_commit-$hostname"
+  git diff --ignore-submodules --exit-code --stat >/dev/null && image_tag="$image_tag-dirty"
+  echo -n $image_tag
+}
+
 failures=""
 for service in $BUILD_SERVICES
   do
+    image_tag_varname="$(get_image_tag_varname "$service")"
+    [ "$image_tag_varname" != "" ] && {
+      original_image_tag="${!image_tag_varname}"
+      service_dir="$script_dir/$(yq -r ".services.${service}.build.context" $script_dir/docker-compose.yaml 2>/dev/null)"
+      [ -d "$service_dir" ] || { show_warning --oneline "Error finding build directory" "$service_dir" ; }
+      is_git_commit "$image_tag_varname" && {
+        export "$image_tag_varname"="$(make_image_tag "$service_dir")"
+        show_warning --oneline "Changing docker image tag" "from $original_image_tag as it appears to be based on a git commit; will use current ${!image_tag_varname} instead"
+      }
+    }
     BUILD_ARGS=
     [ "$service" == "social-link" ] && BUILD_ARGS="--build-arg=LINK_VERSION=$(cd $script_dir/repos/social-app ; git log -1 --format=%H bskylink/) $BUILD_ARGS"
     [ "$service" == "social-card" ] && BUILD_ARGS="--build-arg=CARD_VERSION=$(cd $script_dir/repos/social-app ; git log -1 --format=%H bskyogcard/) $BUILD_ARGS"
