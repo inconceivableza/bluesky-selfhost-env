26ec66548a69c42a97473d63e762fdc7f942cebb
David Fraser <davidf@inconceivable.co.za>
2026-01-07 10:06:53 +0200

    Make yq and jq available on backup server for config parsing

--- selfhost_scripts/backup-scripts/entrypoint.sh
+++ selfhost_scripts/backup-scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install required packages
-apk add --no-cache sqlite tzdata postgresql16-client python3 py3-psycopg-c
+apk add --no-cache sqlite tzdata postgresql16-client python3 py3-psycopg-c jq yq-go
 
 # ensure local repository is initialized
 /scripts/restic-init.sh
